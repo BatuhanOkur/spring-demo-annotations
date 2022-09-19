eb599ed02814d5a3e85f581577405441190fb6ca
Batuhan Okur <batuhanokur2209@gmail.com>
2022-09-19 17:18:07 +0300

    getDailyFortune method has been called

--- src/com/luv2code/springdemo/AnnotationDemoApp.java
+++ src/com/luv2code/springdemo/AnnotationDemoApp.java
@@ -12,10 +12,12 @@ public class AnnotationDemoApp {
 		
 		//get the bean from spring container
 		
-		Coach theCoach = context.getBean("basketballCoach", Coach.class);
+		Coach theCoach = context.getBean("tennisCoach", Coach.class);
 		
 		//call a method on the bean
 		System.out.println(theCoach.getDailyWorkout());
+		
+		System.out.println(theCoach.getDailyFortune());
 		
 		//close the context
 		context.close();
