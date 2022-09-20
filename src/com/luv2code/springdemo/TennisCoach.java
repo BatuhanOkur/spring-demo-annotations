3f091ed4554f32c4a79ed2f52c988243131575bf
Batuhan Okur <batuhanokur2209@gmail.com>
2022-09-20 14:35:23 +0300

    Field injection done

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -1,12 +1,12 @@
 package com.luv2code.springdemo;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired; 
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
 
-	
+	@Autowired
 	private FortuneService fortuneService;
 	
 	/*
@@ -21,7 +21,7 @@ public class TennisCoach implements Coach {
 	{
 		System.out.println(">> TennisCoach: inside default constructor");
 	}
-	
+	/*
 	//define a setter method
 	@Autowired
 	public void doSomeCrazyStuff(FortuneService theFortuneService)
@@ -29,6 +29,7 @@ public class TennisCoach implements Coach {
 		System.out.println(">> TennisCoach: inside doSomeCrazyStuff");
 		fortuneService = theFortuneService;
 	}
+	*/
 	
 	
 	@Override
