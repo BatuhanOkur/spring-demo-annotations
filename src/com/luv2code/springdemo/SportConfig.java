e917867ff1024f61d9e346d06714294e8af6af57
Batuhan Okur <batuhanokur2209@gmail.com>
2022-09-23 16:25:59 +0300

    SportConfig class has been specified as a config class

--- src/com/luv2code/springdemo/SportConfig.java
+++ src/com/luv2code/springdemo/SportConfig.java
@@ -1,5 +1,10 @@
 package com.luv2code.springdemo;
 
-public class SportConfig {
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@ComponentScan("com.luv2code.springdemo")
+public class SportConfig {
+	
 }
