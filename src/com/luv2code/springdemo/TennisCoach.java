8d654202cc3cc2ae73d30b2b7f70b4296c92d1b6
Batuhan Okur <batuhanokur2209@gmail.com>
2022-09-23 15:46:39 +0300

    Bean's scope has been changed by using annotation

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -2,13 +2,15 @@ package com.luv2code.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class TennisCoach implements Coach {
 
 	@Autowired
-	@Qualifier("randomService")
+	@Qualifier("fileFortuneService")
 	private FortuneService fortuneService;
 	
 	/*
