16b29a2f3404b12038d364e6c7bb89bc62109d38
Batuhan Okur <batuhanokur2209@gmail.com>
2022-09-29 15:27:21 +0300

    Email and team variables and their getter methods has been defined

--- src/com/luv2code/springdemo/SwimCoach.java
+++ src/com/luv2code/springdemo/SwimCoach.java
@@ -1,7 +1,15 @@
 package com.luv2code.springdemo;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class SwimCoach implements Coach {
 	private FortuneService fortuneService;
+	
+	@Value("${foo.email}")
+	private String email;
+	
+	@Value("${foo.team}")
+	private String team;
 	
 	public SwimCoach(FortuneService theFortuneService)
 	{
@@ -20,4 +28,13 @@ public class SwimCoach implements Coach {
 		return fortuneService.getFortune();
 	}
 
+	public String getEmail() {
+		return email;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	
 }
